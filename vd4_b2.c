ad0a0a63f5e15de1580522cc0f2f0fcc386c9ce9
datyasuo1 <73332200+datyasuo1@users.noreply.github.com>
2020-10-23 14:38:29 +0700

    lab_1

---
+++ vd4_b2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+ main()
+ {
+  int x,y;
+  x = 5;
+  y = 2;
+    printf("The integers are : %d & %d\n", x, y);
+    printf("The addition gives : %d\n", x + y);
+    printf("The subtraction gives : %d\n", x - y);
+    printf("The mutiplication givers : %d\n", x * y);
+    printf("the division gives : %d\n", x / y);
+    printf("The modulus gives : %d\n", x % y);
+    getchar();
+}
