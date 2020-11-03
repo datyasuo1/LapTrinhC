bce920f2a225873d71304fad2829cdcf709b40f2
datyasuo1 <73332200+datyasuo1@users.noreply.github.com>
2020-11-03 19:16:53 +0700

    lab_10

---
+++ lab10/vd3/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) 
+{
+	incre();
+	incre():
+	incre():
+	}
+	incre()
+	{
+		static char var = 65; 
+		printf("\nthe character stored in var is %c", var++)
+	 } 
+	return 0;
+}
